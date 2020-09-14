9c4046ddd1d615d5c18b542ca77cf0ac07f20249
Paul Krisadaolarn <pauly.k@gmail.com>
2020-09-14 13:36:40 +0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# sv
+
+My svelte boilerplate for testing stuff
---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<style>
+	header {
+        height: 80px;
+        display: grid;
+        place-items: center;
+        background: #444;
+        color: #fff;
+        font-size: 2rem;
+    }
+</style>
+
+<header>
+    <slot>Header</slot>
+</header>
